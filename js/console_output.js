030c45ad7dd3bca9241c89ddc1b3e3bc13d5a3a3
xxy-im <xxygobig@gmail.com>
2021-10-05 16:30:54 +0000

    Update main to output generated at 039ec19

---
+++ js/console_output.js
@@ -0,0 +1,9 @@
+/* 控制台输出 */
+if(window.console) {
+	var cons = console; 
+	if(cons) { 
+		cons.group("Hello There !!!");
+		cons.log("%cI'm xxy", "background-image: linear-gradient(#f39800, #f08300, #eb6238);font-size: 4rem;");
+		cons.groupEnd();
+	}
+}
