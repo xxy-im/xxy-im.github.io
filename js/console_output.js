023484d2561bf6f86d6296db745e735e0b3bc36d
xxy-im <xxygobig@gmail.com>
2021-10-06 13:44:27 +0000

    Update main to output generated at 64d89eb

--- js/console_output.js
+++ js/console_output.js
@@ -4,6 +4,7 @@ if(window.console) {
 	if(cons) { 
 		cons.group("Hello There !!!");
 		cons.log("%cI'm xxy", "background-image: linear-gradient(#f39800, #f08300, #eb6238);font-size: 4rem;");
+		cons.log("My WeChat: xxygobig");
 		cons.groupEnd();
 	}
 }
